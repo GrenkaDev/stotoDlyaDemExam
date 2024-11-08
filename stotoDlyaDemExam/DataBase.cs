3f1111e62fedeb9b5145cf4ee53e49ad282a373f
Your Name <you@example.com>
2024-11-09 08:15:40 +0900

    Добавьте файлы проекта.

---
+++ stotoDlyaDemExam/DataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace stotoDlyaDemExam
+{
+    class DataBase
+    {
+        SqlConnection SqlConnection = new SqlConnection(@"Data Source = DESKTOP-24HL4ATJ;Initial Catalog = nameryyMyyGanenyyn;Integrated Security = True");
+
+        public void openConnection()
+        {
+            if (SqlConnection.State == System.Data.ConnectionState.Closed) { SqlConnection.Open(); }
+        }
+
+        public void closeConnection()
+        {
+            if (SqlConnection.State == System.Data.ConnectionState.Open) { SqlConnection.Close(); }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return SqlConnection;
+        }
+
+    }
+
+}
